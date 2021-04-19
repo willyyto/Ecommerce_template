3b6448d9db644e098197d29fa52d659e0b5b7f53
willyyto <williamto222@gmail.com>
2021-04-20 08:06:13 +1000

    Added Category and Product Model

--- core/settings.py
+++ core/settings.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -112,3 +112,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Setup Media directory reference (for local development)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+@admin.register(Category)
+#Admin configuration for category model
+class CategoryAdmin(admin.ModelAdmin):
+    #Display name and slug on list
+    list_display = ['name', 'slug']
+    #Automatically write into the slug field when name is written
+    prepopulated_fields = {'slug':('name',)}
+    
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'price', 'in_stock',
+                    'is_active', 'created', 'updated']
+    # Add filtering based on fields 
+    list_filter = ['in_stock', 'is_active']
+    list_editable =['price', 'in_stock']
+    prepopulated_fields = {'slug': ('title',)}
+
